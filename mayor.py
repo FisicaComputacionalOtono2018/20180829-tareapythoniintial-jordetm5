87b5da7184571de592cd4d4e56aa1cc07cc07a23
jordetm5 <42181868+jordetm5@users.noreply.github.com>
2018-08-29 22:09:55 -0500

    Add files via upload

---
+++ mayor.py
@@ -0,0 +1,18 @@
+#Jorge Dettle Meza Dominguez
+#29/08/2018
+#imprimir el mayor
+
+x=input("dame el primer numero")
+y=input("dame el segundo numero")
+z=input("dame el tercer numero")
+s=input("dame el cuarto numero")
+
+if x>y and x>z and x>s :
+	print(x)
+if y>x and y>z and y>s :
+	print(y)
+if z>y and z>x and z>s :
+	print(z)
+if s>y and s>z and s>x :
+	print(s)
+
