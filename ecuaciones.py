7f2a0f4c34cdb99ff619c627658f445eff936940
jordetm5 <42181868+jordetm5@users.noreply.github.com>
2018-08-29 22:45:38 -0500

    Add files via upload

---
+++ ecuaciones.py
@@ -0,0 +1,14 @@
+#Jorge Dettle Meza Dominguez
+#29/08/2018
+#solucion de ecuaciones
+a=float(input(" dame el valor de a: "))
+b=float(input(" dame el valor de b: "))
+c=float(input(" dame el valor de c: "))
+d=float(input(" dame el valor de d: "))
+e=float(input(" dame el valor de e: "))
+f=float(input(" dame el valor de f: "))
+
+x=(c*e-b*f)/(a*e-b*d)
+y=(a*f-c*d)/(a*e-b*d)
+print("el valor de x es :",x)
+print("el valor de y es :",y)
