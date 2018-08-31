54675eba98804056cfe4cd832fd5fa6a5f84f7f9
jordetm5 <42181868+jordetm5@users.noreply.github.com>
2018-08-30 20:55:20 -0500

    Add files via upload

--- vel.py
+++ vel.py
@@ -1,19 +1,59 @@
 #Jorge Dettle Meza Dominguez
 #29/08/2018
 #velocidad
-count=1
-stringinput = input("dame las distancias y los tiempos en el formato(m1,t1) use o,o para finalizar")
 
-print(stringinput)
 
-stringinput = stringinput.replace(')(',' ').replace(')','').replace(',',' ') 
+
+def c(x):
+    for i in range(0, len(x)):
+        x[i]=x[i]*60
+    return x
+def v(x):
     
-print(stringinput) 
+    for i in range (0, (len(x)-1)):
+        x[i]=(1500.0/x[i])
 
+    return x
+
+def promedio(x):
+	d=0
+	s=0.0
+	for i in range(0, len(x)):
+		d=d+1
+		s=s+x[i]
+		if x[i] == 0:
+			break
+	return (s/(d-1))
+
+count=1
+
+stringinput = input("dame los tiempos en minutos y segundos en el formato(m1,s1) use (0,0) para finalizar  ")
+
+stringinput = stringinput.replace(')(',' ').replace(')','').replace(',',' ').replace('(','')
+    
+m=[]
 for i in stringinput:
-     print(i)
-     if count%2!=0:
-         i=i/60
+    if i!=' ':
+        m.append(int(i))
+            
      
-print(stringinput)
+o=[]
+p=[]
+for i in m :
+    if count%2== 0:
+        o.append(i)
+    else :
+        p.append(i)
+    count=count+1
 
+n=c(p)
+q=[]
+
+ 
+for i in range(0, len(n)):
+	q.append(n[i]+o[i])
+
+r=v(q)
+print(r)
+
+print(promedio(r))
